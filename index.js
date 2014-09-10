194d78d4f90fa18234c4c0816492054fe7351b54
JoePeacock <jpeacockinc@gmail.com>
2014-09-09 23:27:42 -0400

    added in static files

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+app.use(express.logger());
+app.set("view options", {layout: false});
+app.use(express.static(__dirname + '/views'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  res.render('/views/index.html');
 })
 
 app.listen(app.get('port'), function() {
